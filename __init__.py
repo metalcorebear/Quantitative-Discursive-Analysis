35fa88bc7f33a4ff2bd72477d7d207cbf75ce449
metalcorebear <43913302+metalcorebear@users.noreply.github.com>
2019-11-11 15:29:49 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+import os
+
+__version__ = '0.0.1'
+__license__ = 'MIT'
+__author__ = 'metalcorebear'
+
+name = "QDA"
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+@author: metalcorebear
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="QDA",
+    version="0.0.1",
+    author="metalcorebear",
+    author_email="mark.mbailey@gmail.com",
+    description="A tool for quantitatively measuring the discursive similarity between bodies of text.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/metalcorebear/Quantitative-Discursive-Analysis",
+    packages=setuptools.find_packages(),
+    install_requires=['networkx', 'textblob', 'numpy'],
+    py_modules=["QDA"],
+    package_data={},
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
